82795e55e716a3afa3ebb47b488bf4915db1299f
unknown <abhijitbashetti@gmail.com>
2019-03-03 15:59:33 +0530

    message

---
+++ UniWeed/src/main/java/com/upl/weed/UniWeedApplication.java
@@ -0,0 +1,13 @@
+package com.upl.weed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniWeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniWeedApplication.class, args);
+	}
+
+}
